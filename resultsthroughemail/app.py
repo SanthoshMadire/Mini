446336a08143dc924bb823d3f940bd56c7e6865b
SanthoshMadire <74131307+SanthoshMadire@users.noreply.github.com>
2022-01-07 23:43:05 +0530

    Add files via upload

---
+++ resultsthroughemail/app.py
@@ -0,0 +1,63 @@
+from email import message
+from types import MethodType
+from flask import Flask,render_template,request,redirect, url_for
+from flask_mail import Mail,Message
+import csv,smtplib, ssl
+import pandas as pd
+from email.message import EmailMessage
+
+app=Flask(__name__)
+mail=Mail(app)
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username=request.form["username"]
+        password=request.form["password"]
+        if  username!= 'admin' or password != 'admin@cvr123':
+            error = 'Invalid Credentials. Please try again.\n!!!ADMIN ACCESS ONLY!!!'
+            return render_template('home.html', error=error)  
+        else:  
+            return render_template('login.html')  
+
+@app.route('/sendmail',methods=['GET', 'POST'])
+def getcsv():
+    if request.method == 'POST':
+       subject='Dear {name}, your Semester marks '
+       body= '\n CVR COLLEGE OF ENGINEERING \n\n {sub1} = {s1} \n {sub2} = {s2} \n {sub3} = {s3} \n {sub4} = {s4} \n {sub5} = {s5} \n Your Obtained Marks are :{total} \n Status : {status} \n Regards, \n cvr.ac.in \n'
+       message = f'Subject: {subject}\n\n{body}'
+      
+       from_address = 'resultsthroughemail@gmail.com'
+       password = '6305077750'
+       context = ssl.create_default_context()
+       with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+          server.login(from_address, password)
+          f= request.files['datacsv']    
+          f.save(f.filename)
+          with open(f.filename) as file:
+              reader = csv.reader(file)
+              next(reader)  # Skip header row
+              for ID,NAME,BRANCH,SECTION,SUB1,S1,SUB2,S2,SUB3,S3,SUB4,S4,SUB5,S5,TOTAL,STATUS,EMAIL in reader:
+                   server.sendmail(from_address,EMAIL,message.format(name=NAME,sub1=SUB1,s1=S1,sub2=SUB2,s2=S2,sub3=SUB3,s3=S3,sub4=SUB4,s4=S4,sub5=SUB5,s5=S5,total=TOTAL,status=STATUS),)
+       f=request.files['datacsv']
+       results=[]
+       with open(f.filename) as file:
+           csvfile=csv.reader(file)
+           for row in csvfile:
+               results.append(row)
+       results=pd.DataFrame(results)
+       return render_template('details.html',results=results.to_html(header=False,index=False))
+     
+@app.route('/logout',methods=['GET', 'POST'])
+def logout():
+    return redirect(url_for('index'))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
